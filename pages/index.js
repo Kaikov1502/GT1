bf17669178add2990602a44a57ae08c9ce0abccd
Kaikov1502 <kaikov@gmail.com>
2025-04-08 14:55:25 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-// index.js – redirect to /admin
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/admin");
+  }, [router]);
+  return null;
+}
